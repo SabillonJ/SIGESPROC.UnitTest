5264e1813b3fafa094e0bc482100c0f01326a055
Alesoliss <159864507+Alesoliss@users.noreply.github.com>
2024-09-24 10:17:36 -0600

    fix(UnitTest)

--- SIGESPROC.UnitTest/Controllers/CotizacionTest.cs
+++ SIGESPROC.UnitTest/Controllers/CotizacionTest.cs
@@ -5,13 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using SIGESPROC.Common.Models;
 using SIGESPROC.BusinessLogic.Services.ServiceInsumo;
+using SIGESPROC.BusinessLogic.Services.ServicePlanilla;
+using SIGESPROC.BusinessLogic.Services.ServiceGeneral;
+using SIGESPROC.BusinessLogic.Services.ServiceProyecto;
+
 using SIGESPROC.API.Controllers;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using SIGESPROC.API.Controllers.ControllersInsumo;
+using SIGESPROC.API.Controllers.ControllersPlanilla;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SIGESPROC.Common.Models.ModelsInsumo;
 using SIGESPROC.UnitTest.Mocks;
+using SIGESPROC.BusinessLogic.Services.GeneralService;
 
 namespace SIGESPROC.UnitTest.Controllers
 {
@@ -19,6 +26,14 @@ namespace SIGESPROC.UnitTest.Controllers
     public class CotizacionTest
     {
         private InsumoService _insumoService;
+        private PlanillaService _planillaService;
+        private FrecuenciaService _frecuenciaService;
+        private PrestamoService _prestamoService;
+        private DeduccionService _deduccionService;
+        private GeneralService _generalService;
+
+
+
         public static IMapper _mapper;
 
         public List<CotizacionViewModel> cotizacionViewModel { get; set; }
@@ -30,8 +45,19 @@ namespace SIGESPROC.UnitTest.Controllers
             CotizacionController cotizacionController = new CotizacionController(_insumoService, _mapper);
             var result = cotizacionController.ListarCotizacion();
             Assert.IsNotNull(result);
-            Assert.IsInstanceOfType(result, typeof (IActionResult));
+            Assert.IsInstanceOfType(result, typeof(IActionResult));
         }
+
+        [TestMethod]
+
+        public void ListarPlanilla()
+        {
+            PlanillaController planillaController = new PlanillaController(_planillaService, _mapper, _generalService, _frecuenciaService, _prestamoService, _deduccionService);
+            var result = planillaController.ListarPlanilla();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IActionResult));
+        }
+
 
 
         [TestMethod]
